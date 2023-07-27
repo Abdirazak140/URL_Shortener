ead5569eb9eaf6ebec90288ded3b946c3f6ff6d3
Abdirazak140 <abdirazak140@gmail.com>
2023-07-26 21:54:38 -0400

    Final commit - Completed URL shortener project
    1. Users can enter any URL to create a shorter version of it.
    2. Redirects to original URL when shortened URL is accessed.
    3. Implemented SQLite for database handling.
    4. Error handling

--- app.py
+++ app.py
@@ -1,45 +1,69 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import random
+
+app = Flask(__name__)
 
 conn = sqlite3.connect('nano_path.db')
 c = conn.cursor()
 
-app = Flask(__name__)
-
 c.execute('''
-    CREATE TABLE Nano_Path(
-        short_url TEXT PRIMARY KEY,
+    CREATE TABLE IF NOT EXISTS Path(
+        url_id TEXT PRIMARY KEY,
         original_url TEXT
     )
 ''')
+
+conn.commit()
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
     if request.method == 'POST':
-        pass
+        original_url = request.form['original_url']
+        url_id = create_short_url(original_url)
+        current_url = request.url
+        short_url = current_url + url_id
+
+        return redirect(url_for('generated_url', short_url=short_url))
+
     else:
         return render_template('home.html')
 
 
-@app.route('/short_url_generated')
+@app.route('/generated_url')
 def generated_url():
-    return render_template('short_url.html')
+    short_url = request.args.get('short_url')
+    return render_template('short_url.html', short_url=short_url)
+
+
+@app.route('/<url_id>')
+def redirect_user(url_id):
+    return redirects(url_id)
 
 
 def create_short_url(original_url):
+    conn = sqlite3.connect('nano_path.db')
+    c = conn.cursor()
+
     characters = "ABCEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     url_id = "".join(random.choice(characters) for _ in range(6))
-
-    c.execute("INSERT INTO Nano_Path (short_url, original_url) VALUES (?, ?)", (url_id, original_url))
-    c.commit()
+    c.execute("INSERT INTO Path (url_id, original_url) VALUES (?, ?)", (url_id, original_url))
+    conn.commit()
 
     return url_id
 
 
-def redirects(short_url):
-    c.execute("")
+def redirects(url_id):
+    conn = sqlite3.connect('nano_path.db')
+    c = conn.cursor()
+
+    c.execute("SELECT original_url from Path where url_id = (?)", (url_id,))
+    result = c.fetchone()
+    if result is not None:
+        return redirect(result[0])
+    else:
+        return "Url Not Found", 404
 
 
 if __name__ == '__main__':
